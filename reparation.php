5336fce7461312c44e9a4dff44849065048219ee
Mohammad ALQADI <47479460+AlqadiPal@users.noreply.github.com>
2019-11-18 16:07:21 +0100

    hi

---
+++ reparation.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<meta charset="utf-8">
+	<title></title>
+
+   
+  
+    </head>
+    <body>
+
+        <?php
+           include 'navbar.php' ;
+        ?> 
+
+        <div class="container">
+        
+            <header class="row">
+                <div class="col-lg-12">
+                    Header
+                </div>
+            </header>
+            
+            <div class="row">
+            
+                <div class="col-lg-2">
+                    <div class="row">
+                        <aside class="col-lg-12">
+                            Aside
+                        </aside>
+                        <aside class="col-lg-12">
+                            Aside
+                        </aside>
+                    </div>
+                </div>
+        
+                <section class="col-lg-8">
+                    
+                    <img src="images/reparation_mini.jpg"  alt="reperation">
+                </section>
+        
+                <div class="col-lg-2">
+                    <div class="row">
+                        <aside class="col-lg-12">
+                            Aside
+                        </aside>
+                        <aside class="col-lg-12">
+                            Aside
+                        </aside>
+                    </div>
+                </div>
+                
+            </div>
+                    
+            <footer class="row">
+                <div class="col-lg-12">
+                    Footer
+                </div>
+            </footer>
+                
+        </div>
+    </body>
+</html>
+<?php
+    include 'footer.php' ;
+    ?>
